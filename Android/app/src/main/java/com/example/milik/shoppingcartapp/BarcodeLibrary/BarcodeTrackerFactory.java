e7cbc05f47d0f2b4272b6aaab3d5901662c6bc12
MilikSmith76 <miliksmith@jankelley.com>
2017-03-05 11:48:24 -0500

    Android Update

---
+++ Android/app/src/main/java/com/example/milik/shoppingcartapp/BarcodeLibrary/BarcodeTrackerFactory.java
@@ -0,0 +1,20 @@
+package com.example.milik.shoppingcartapp.BarcodeLibrary;
+
+import android.content.Context;
+
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.barcode.Barcode;
+
+public class BarcodeTrackerFactory implements MultiProcessor.Factory<Barcode> {
+    private Context mContext;
+
+    BarcodeTrackerFactory(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public Tracker<Barcode> create(Barcode barcode) {
+        return new BarcodeTracker(mContext);
+    }
+}
--- Android/app/src/main/java/com/example/milik/shoppingcartapp/MainActivity.java
+++ Android/app/src/main/java/com/example/milik/shoppingcartapp/MainActivity.java
@@ -1,52 +1,51 @@
 package com.example.milik.shoppingcartapp;
 
+import android.content.Intent;
+import android.graphics.Point;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.os.Bundle;
-import android.util.SparseArray;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
-import com.google.android.gms.vision.Frame;
+
+
+import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.example.milik.shoppingcartapp.BarcodeLibrary.BarcodeCaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final int BARCODE_READER_REQUEST_CODE = 1;
+
+    private TextView mResultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mResultTextView = (TextView) findViewById(R.id.result_textview);
 
-        BarcodeDetector detector = new BarcodeDetector.Builder(getApplicationContext()).setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE).build();
-        Bitmap myBitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.puppy);
-        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
-        SparseArray<Barcode> barcodes = detector.detect(frame);
-
-        Barcode thisCode = barcodes.valueAt(0);
-        TextView txtView = (TextView) findViewById(R.id.txtContent);
-        txtView.setText(thisCode.rawValue);
-
-        Button btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button scanBarcodeButton = (Button) findViewById(R.id.scan_barcode_button);
+        scanBarcodeButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), BarcodeCaptureActivity.class);
+                startActivityForResult(intent, BARCODE_READER_REQUEST_CODE);
             }
         });
+    }
 
-        ImageView myImageView = (ImageView) findViewById(R.id.imgview);
-
-        myImageView.setImageBitmap(myBitmap);
-
-        if(!detector.isOperational()){
-            txtView.setText("Could not set up the detector!");
-            return;
-        }
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BARCODE_READER_REQUEST_CODE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+                    Point[] p = barcode.cornerPoints;
+                    mResultTextView.setText(barcode.displayValue);
+                } else mResultTextView.setText("No barcode found");
+            } else Log.e(LOG_TAG, "Error occurred");
+        } else super.onActivityResult(requestCode, resultCode, data);
     }
 }
